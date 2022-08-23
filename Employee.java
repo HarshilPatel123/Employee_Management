d4665ccfa958e1495c047df42ed436a7c72ad63f
HarshilPatel123 <109896705+HarshilPatel123@users.noreply.github.com>
2022-08-23 10:51:37 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -8,21 +8,52 @@ Public class Employee{
           Private String department;
           Private double salary;
 
-Public int getid(){
-     return id;
-}
-Public void setid(){
-     this.id = id;
-}
-Public int getname(){
-     return name;
-}
-Public void setname(){
-     this.name = name ;
-}
+         Public void setid(){
+              this.id = id;
+         }
+         Public String getname(){
+              return name;
+         }
+         Public void setname(){
+              this.name = name ;
+          }
+         Public int getage(){
+              return age;
+          }
+         Public void setage(){
+              this.age = age;
+          } 
+         Public String getdesignation(){
+              return designation;
+          }
+         Public void setdesignation(){
+              this.designation = designation;
+          }
 
-}
-
-
-
+         Public String getdepartment(){
+              return department;
+          }
+         Public void setdepartment(){
+               this.department = department;
+          }
+          Public double getsalary(){
+              return salary;
+          }
+         Public void setsalary(){
+              this.salary = salary;
+          }
+         @Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", desiganation=" + desiganation
+				+ ", department=" + department + ", salary=" + salary + "]";
+	}
+        public Employee(int id, String name, int age, String desiganation, String department, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.desiganation = desiganation;
+		this.department = department;
+		this.salary = salary;
+	}
 }     
