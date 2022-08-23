64dd992a511437ab25813b08aced69c008c09dd8
HarshilPatel123 <109896705+HarshilPatel123@users.noreply.github.com>
2022-08-23 10:21:25 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,28 @@
+Package com.EmpManagement
+ 
+Public class Employee{
+          Private int id;
+          Private String name;
+          Private int age;  
+          Private String designation;
+          Private String department;
+          Private double salary;
+
+Public int getid(){
+     return id;
+}
+Public void setid(){
+     this.id = id;
+}
+Public int getname(){
+     return name;
+}
+Public void setname(){
+     this.name = name ;
+}
+
+}
+
+
+
+}     
