ee1ffb37c5e28bdeebbfeaa9e528f3a684353223
HarshilPatel123 <109896705+HarshilPatel123@users.noreply.github.com>
2022-08-23 16:01:19 +0530

    Update EmployeeMain.java

--- EmployeeMain.java
+++ EmployeeMain.java
@@ -1,1 +1,49 @@
+Package com.EmpManagement;
 
+import java.util.Scanner;
+
+Public class main{
+
+     EmployeeData d = new EmployeeData();
+
+     public static void menu(){
+          System.out.println(\n1.Add Employee details);
+          System.out.println(\n2.view Employee details);
+          System.out.println(\n3.Update Employee details);
+          System.out.println(\n4.delete Employee);
+          System.out.println(\n5.view all Employee details);
+          System.out.println(\n6.exit);
+     }
+     
+     public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+
+        do{
+            menu();
+            int choice;
+            System.out.println(Enter your choice);
+            Choice = sc.nextInt();
+            switch(choice){
+
+
+
+
+
+        }
+
+
+
+
+        }
+
+
+     
+ }
+
+
+     
+      
+
+
+
+}
